f4c682c3aa11c9d29ce164d2fdd645c6a9d40154
leanbs <davin.yu1110@gmail.com>
2018-03-01 12:01:36 +0700

    convert image to grayscale and print ascii :sparkles:

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const canvas = document.querySelector('#preview');
 const fileInput = document.querySelector('input[type="file"]');
+const asciiImage = document.querySelector('#ascii');
 
 const context = canvas.getContext('2d');
+
+// grayscale formula : https://en.wikipedia.org/wiki/Grayscale
+const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
 
 fileInput.onchange = (e) => {
   const file = e.target.files[0];
@@ -9,11 +13,54 @@ fileInput.onchange = (e) => {
   reader.onload = (ev) => {
     const image = new Image();
     image.onload = () => {
-      canvas.width = image.width;
-      canvas.height = image.height;
-      context.drawImage(image, 0, 0);
+      const [width, height] = [image.width, image.height];
+
+      canvas.width = width;
+      canvas.height = height;
+
+      context.drawImage(image, 0, 0, width, height);
+      const grayScales = convertToGrayScales(context, width, height);
+
+      fileInput.style.display = 'none';
+      drawAscii(grayScales, width);
     }
     image.src = ev.target.result;
   };
   reader.readAsDataURL(file);
 }
+
+const convertToGrayScales = (context, width, height) => {
+  const imageData = context.getImageData(0, 0, width, height);
+  const grayScales = [];
+
+  for (let i = 0; i < imageData.data.length; i+=4) {
+    const [r, g, b] = [imageData.data[i], imageData.data[i +1], imageData.data[i+2]];
+
+    const grayScale = toGrayScale(r, g, b);
+    imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = grayScale;
+    grayScales.push(grayScale);
+  }
+  context.putImageData(imageData, 0, 0);
+  return grayScales;
+}
+
+const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
+const rampLength = grayRamp.length;
+
+// the grayScale value is an integer ranging from 0 (black) to 255 (white)
+const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
+
+const drawAscii = (grayScales, width) => {
+  const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
+    let nextChars = getCharacterForGrayScale(grayScale);
+
+    if ((index + 1) % width === 0) {
+      nextChars += '\n';
+    }
+
+    return asciiImage + nextChars;
+
+  }, '');
+
+  asciiImage.textContent = ascii;
+};
