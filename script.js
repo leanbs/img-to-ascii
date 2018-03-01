716ba04e2469eaae3696476b4fc0cfbf07019ea4
leanbs <davin.yu1110@gmail.com>
2018-03-01 12:15:57 +0700

    reduce font size to get better picture :racehorse:

--- script.js
+++ script.js
@@ -70,15 +70,31 @@ const MAXIMUM_WIDTH = 80;
 const MAXIMUM_HEIGHT = 50;
 
 const clampDimensions = (width, height) => {
+  const rectifiedWidth = Math.floor(getFontRatio() * width);
+
   if (height > MAXIMUM_HEIGHT) {
-    const reducedWidth = Math.floor(width * MAXIMUM_HEIGHT / height);
+    const reducedWidth = Math.floor(rectifiedWidth * MAXIMUM_HEIGHT / height);
     return [reducedWidth, MAXIMUM_HEIGHT];
   }
 
   if (width > MAXIMUM_WIDTH) {
-    const reducedHeight = Math.floor(width * MAXIMUM_WIDTH / width);
+    const reducedHeight = Math.floor(height * MAXIMUM_WIDTH / rectifiedWidth);
     return [MAXIMUM_WIDTH, reducedWidth];
   }
 
-  return [width, height];
+  return [rectifiedWidth, height];
 }
+
+const getFontRatio = () => {
+  const pre = document.createElement('pre');
+  pre.style.display = 'inline';
+  pre.textContent = ' ';
+
+  document.body.appendChild(pre);
+  const { width, height } = pre.getBoundingClientRect();
+  document.body.removeChild(pre);
+
+  return height / width;
+}
+
+const fontRatio = getFontRatio();
